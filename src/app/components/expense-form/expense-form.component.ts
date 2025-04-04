ef6fe657c914a7b6c83a64b42dfcf409991efae5
vityudi <vitoryudi.artur@gmail.com>
2025-04-04 00:27:05 -0300

    Implementa funcionalidades básicas do app de finanças

---
+++ src/app/components/expense-form/expense-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-form',
+  imports: [],
+  templateUrl: './expense-form.component.html',
+  styleUrl: './expense-form.component.scss'
+})
+export class ExpenseFormComponent {
+
+}
---
+++ src/app/components/expenses-list/expenses-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expenses-list',
+  imports: [],
+  templateUrl: './expenses-list.component.html',
+  styleUrl: './expenses-list.component.scss'
+})
+export class ExpensesListComponent {
+
+}
